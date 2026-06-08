dd195ff11f91d53343ae90cf2ccb7ab18bec795e
John Zaia <john.zaia@outlook.com>
2026-06-07 23:03:19 -0400

    test

---
+++ TetrisClone/main.cpp
@@ -0,0 +1,104 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <stdio.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+void renderTetrisGrid()
+{
+
+    SDL_FRect outlineRect = { 50, 50, 200, 400 };
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 1.0);
+
+    SDL_RenderRect(renderer, &outlineRect);
+
+    float x1 = 50.0f;
+    float y1 = 70.0f;
+    float x2 = 250.0f;
+    float y2 = 70.0f;
+
+    //Render horizontal lines
+    for (int i = 0; i < 19; i++)
+    {
+        SDL_RenderLine(renderer, x1, y1, x2, y2);
+        y1 += 20.0f;
+        y2 += 20.0f;
+    }
+
+    //Render vertical lines
+    float vx1 = 70.0f;
+    float vy1 = 50.0f;
+    float vx2 = 70.0f;
+    float vy2 = 450.0f;
+    for (int i = 0; i < 9; i++)
+    {
+        SDL_RenderLine(renderer, vx1, vy1, vx2, vy2);
+        vx1 += 20.0f;
+        vx2 += 20.0f;
+    }
+}   
+
+int main(int argc, char* argv[]) {
+
+ 
+    bool done = false;
+
+    SDL_Init(SDL_INIT_VIDEO);        
+
+
+    window = SDL_CreateWindow(
+        "An SDL3 window",                 
+        640,                            
+        480,                      
+        SDL_WINDOW_OPENGL       
+    );
+
+    renderer = SDL_CreateRenderer(window, NULL);
+
+    if (window == NULL) {
+
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+
+    while (!done) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                done = true;
+            }
+            /*
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+            SDL_Vertex vertices[4] = {
+                {{100, 100}, {255, 0, 0, 255}, {0,0}},//top left
+                {{200, 100}, {255, 0, 0, 255}, {0,0}},//top right
+                {{100, 50},  {255, 0, 0, 255}, {0,0}},//bottom left
+                {{200, 50},  {255, 0, 0, 255}, {0,0}}//bottom right
+            };
+
+            int indices[6] = {
+                0, 2, 3, //first triangle
+                0, 1, 3 //second triangle
+            };
+
+            SDL_RenderGeometry(renderer, nullptr, vertices, 4, indices, 6);
+            */
+        }
+        SDL_RenderPresent(renderer);
+
+        renderTetrisGrid();
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+    return 0;
+}
